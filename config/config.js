e74ea76608a55515524e8b994c59581085b79f03
kaikai <90939944+kywithsweet@users.noreply.github.com>
2022-01-30 20:15:38 -0500

    Update config.js

--- config/config.js
+++ config/config.js
@@ -269,6 +269,12 @@ export default defineConfig({
           path: '/account/center',
           component: './account/center',
         },
+        {
+          name: 'settings',
+          icon: 'smile',
+          path: '/account/settings',
+          component: './account/settings',
+        },
       ],
     },
     {

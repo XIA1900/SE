d40008f9eaebda5926b80f8ab9dc168ebf2535d4
xiayuwei <xiayuwei0226@yahoo.com>
2022-02-01 00:09:13 -0500

    Update

--- config/defaultSettings.js
+++ config/defaultSettings.js
@@ -7,7 +7,7 @@ const Settings = {
   fixedHeader: false,
   fixSiderbar: true,
   colorWeak: false,
-  title: 'Ant Design Pro',
+  title: 'Road Center',   //original: Ant Design Pro
   pwa: false,
   logo: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',
   iconfontUrl: '',

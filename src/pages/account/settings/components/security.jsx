cfe30091dbe3f812f63496a5ac8dcf40d57a38a3
kaikai <90939944+kywithsweet@users.noreply.github.com>
2022-02-02 14:35:22 -0500

    Update security.jsx

--- src/pages/account/settings/components/security.jsx
+++ src/pages/account/settings/components/security.jsx
@@ -1,43 +1,34 @@
 import React from 'react';
 import { List } from 'antd';
 const passwordStrength = {
-  strong: <span className="strong">强</span>,
-  medium: <span className="medium">中</span>,
-  weak: <span className="weak">弱 Weak</span>,
+  strong: <span className="strong">strong</span>,
+  medium: <span className="medium">medium</span>,
+  weak: <span className="weak"weak</span>,
 };
 
 const SecurityView = () => {
   const getData = () => [
     {
-      title: '账户密码',
+      title: 'password',
       description: (
         <>
-          当前密码强度：
+          current password strength：
           {passwordStrength.strong}
         </>
       ),
-      actions: [<a key="Modify">修改</a>],
+      actions: [<a key="Modify">edit</a>],
     },
     {
-      title: '密保手机',
-      description: `已绑定手机：138****8293`,
-      actions: [<a key="Modify">修改</a>],
+      title: 'phone',
+      description: `35*******7`,
+      actions: [<a key="Modify">edit</a>],
     },
     {
-      title: '密保问题',
-      description: '未设置密保问题，密保问题可有效保护账户安全',
-      actions: [<a key="Set">设置</a>],
+      title: 'email',
+      description: `*******@ufl.edu`,
+      actions: [<a key="Modify">edit</a>],
     },
-    {
-      title: '备用邮箱',
-      description: `已绑定邮箱：ant***sign.com`,
-      actions: [<a key="Modify">修改</a>],
-    },
-    {
-      title: 'MFA 设备',
-      description: '未绑定 MFA 设备，绑定后，可以进行二次确认',
-      actions: [<a key="bind">绑定</a>],
-    },
+    
   ];
 
   const data = getData();

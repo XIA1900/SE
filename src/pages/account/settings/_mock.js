073a37b3bec49bacdcd5eca1113fb660f94a6400
kaikai <90939944+kywithsweet@users.noreply.github.com>
2022-02-03 21:53:08 -0500

    Update _mock.js

--- src/pages/account/settings/_mock.js
+++ src/pages/account/settings/_mock.js
@@ -18,37 +18,25 @@ function getCity(req, res) {
 function getCurrentUse(req, res) {
   return res.json({
     data: {
-      name: 'Serati Ma',
+      name: 'Silvia',
       avatar: 'https://gw.alipayobjects.com/zos/rmsportal/BiazfanxmamNRoxxVxka.png',
       userid: '00000001',
-      email: 'antdesign@alipay.com',
-      signature: '海纳百川，有容乃大',
-      title: '交互专家',
-      group: '蚂蚁金服－某某某事业群－某某平台部－某某技术部－UED',
+      email: 'Silvia@ufl.edu',
+      signature: '',
+      title: 'UF23',
+      group: 'INS:',
       tags: [
         {
           key: '0',
-          label: '很有想法的',
+          label: 'dancing',
         },
         {
           key: '1',
-          label: '专注设计',
+          label: 'traveling',
         },
         {
           key: '2',
-          label: '辣~',
-        },
-        {
-          key: '3',
-          label: '大长腿',
-        },
-        {
-          key: '4',
-          label: '川妹子',
-        },
-        {
-          key: '5',
-          label: '海纳百川',
+          label: 'cooking',
         },
       ],
       notifyCount: 12,
@@ -56,16 +44,16 @@ function getCurrentUse(req, res) {
       country: 'China',
       geographic: {
         province: {
-          label: '浙江省',
+          label: 'FL-Florida',
           key: '330000',
         },
         city: {
-          label: '杭州市',
+          label: 'Gaineville',
           key: '330100',
         },
       },
-      address: '西湖区工专路 77 号',
-      phone: '0752-268888888',
+      address: '330 Newell Dr.',
+      phone: '+1-3527918045',
     },
   });
 } // 代码中会兼容本地 service mock 以及部署站点的静态数据

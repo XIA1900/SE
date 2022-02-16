f3922c85ab27b12546105ad5f328e5eefa0cd879
xiayuwei <xiayuwei0226@yahoo.com>
2022-02-16 12:29:12 -0500

    update a little

--- src/locales/en-US/pages.js
+++ src/locales/en-US/pages.js
@@ -1,6 +1,5 @@
 export default {
-  'pages.layouts.userLayout.title':
-    'Gator Forum is a project created by Road Center',
+  'pages.layouts.userLayout.title': 'Gator Forum is a project created by Road Center',
   'pages.login.accountLogin.tab': 'Account Login',
   'pages.login.accountLogin.errorMessage': 'Incorrect username/password(admin/ant.design)',
   'pages.login.failure': 'Login failed, please try again!',

0aa26d084bf4d74ba1b1560947bcadcd445f7260
xiayuwei <xiayuwei0226@yahoo.com>
2022-02-17 22:12:57 -0500

    update

--- src/pages/user/Login/index.jsx
+++ src/pages/user/Login/index.jsx
@@ -102,14 +102,6 @@ const Login = () => {
                 defaultMessage: '账户密码登录',
               })}
             />
-            {/*}
-            <Tabs.TabPane
-              key="mobile"
-              tab={intl.formatMessage({
-                id: 'pages.login.phoneLogin.tab',
-                defaultMessage: '手机号登录',
-              })}
-            />*/}
           </Tabs>
 
           {status === 'error' && loginType === 'account' && (

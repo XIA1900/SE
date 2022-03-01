ae119e0490356bdd4b0a53d0795d20a90decb171
xiayuwei <xiayuwei0226@yahoo.com>
2022-03-01 17:31:45 -0500

    update

--- mock/create.js
+++ mock/create.js
@@ -1,20 +1,18 @@
 export default {
-    'POST  /api/createGroup': (req, res) => {
-      console.log("params");
-      console.log(req.query);
-      res.send({
-        data: {
-          message: 'Ok',
-        },
-      });
-    },
-    'POST  /api/createPost': (req, res) => {
-        res.send({
-          data: {
-            message: 'Ok',
-          },
-        });
+  'POST  /api/createGroup': (req, res) => {
+    console.log('{{{{{{{{{{{params');
+    console.log(req.query);
+    res.send({
+      data: {
+        message: 'Ok',
       },
-  };
-
-  
+    });
+  },
+  'POST  /api/createPost': (req, res) => {
+    res.send({
+      data: {
+        message: 'Ok',
+      },
+    });
+  },
+};
--- src/services/create.js
+++ src/services/create.js
@@ -1,13 +1,15 @@
 import { request } from 'umi';
 
-export async function createGroup(params) {   
-    return request('/api/createGroup', {
-      data: params,
-      method: 'POST',
-    });
+export async function createGroup(params) {
+  console.log('----params:');
+  console.log(params);
+  return request('/api/createGroup', {
+    data: params,
+    method: 'POST',
+  });
 }
 
-export async function createPost(params) {  
+export async function createPost(params) {
   return request('/api/createPost', {
     data: params,
     method: 'POST',

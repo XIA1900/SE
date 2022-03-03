2eed25b8eb52ad1f6beacde014d8e26f220b52e9
fongziyjun16 <fongziyjun16@hotmail.com>
2022-03-03 18:26:21 -0500

    update

--- GFBackend/controller/DTO.go
+++ GFBackend/controller/DTO.go
@@ -1,8 +1,8 @@
 package controller
 
 type ResponseMsg struct {
-	Code    int    `json:"code" example:"200"`
-	Message string `json:"message" example:"process successfully"`
+	Code    int    `form:"Code" json:"code" example:"200"`
+	Message string `form:"Message" json:"message" example:"process successfully"`
 }
 
 type CommunityResponseMsg struct {
@@ -16,18 +16,18 @@ type CommunityResponseMsg struct {
 }
 
 type UserInfo struct {
-	Username    string `json:"Username" example:"jamesbond21"`
-	Password    string `json:"Password" example:"f9ae5f68ae1e7f7f3fc06053e9b9b539"`
-	NewPassword string `json:"NewPassword" example:"3ecb441b741bcd433288f5557e4b9118"`
-	ForAdmin    bool   `json:"ForAdmin" example:true`
+	Username    string `form:"Username" json:"Username" example:"jamesbond21" `
+	Password    string `form:"Password" json:"Password" example:"f9ae5f68ae1e7f7f3fc06053e9b9b539"`
+	NewPassword string `form:"NewPassword" json:"NewPassword" example:"3ecb441b741bcd433288f5557e4b9118"`
+	ForAdmin    bool   `form:"ForAdmin" json:"ForAdmin" example:true`
 }
 
 type NewUserInfo struct {
-	Username   string `json:"Username" example:"jamesbond21"`
-	Nickname   string `json:"Nickname" example:"Peter Park"`
-	Birthday   string `json:"Birthday" example:"2022-02-30"`
-	Gender     string `json:"Gender" example:"male/female/unknown"`
-	Department string `json:"Department" example:"CS:GO"`
+	Username   string `form:"Username" json:"Username" example:"jamesbond21"`
+	Nickname   string `form:"Nickname" json:"Nickname" example:"Peter Park"`
+	Birthday   string `form:"Birthday" json:"Birthday" example:"2022-02-30"`
+	Gender     string `form:"Gender" json:"Gender" example:"male/female/unknown"`
+	Department string `form:"Department" json:"Department" example:"CS:GO"`
 }
 
 type CommunityInfo struct {
@@ -39,5 +39,5 @@ type CommunityInfo struct {
 
 type UserFiles struct {
 	ResponseMsg
-	Filenames []string `json:"Filenames" example:"\"xxx.jpg\",\"xxx.png\",\"xxx.gif\""`
+	Filenames []string `form:"Filenames" json:"Filenames" example:"\"xxx.jpg\",\"xxx.png\",\"xxx.gif\""`
 }
--- GFBackend/test/UserService_test.go
+++ GFBackend/test/UserService_test.go
@@ -1,17 +1,49 @@
 package test
 
 import (
-	"GFBackend/model/dao"
-	"GFBackend/service"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
 	"testing"
+	"unsafe"
 )
 
-var userManageService = service.NewUserManageService(dao.NewUserDAO(), dao.NewFollowDAO(), dao.NewSpaceDAO())
+func TestUserLogin(t *testing.T) {
+	type UserInfo struct {
+		Username string `json:"Username"`
+		Password string `json:"Password"`
+	}
 
-func TestUserRegister(t *testing.T) {
-	err := userManageService.Register("bird", "007", false)
-	if err != nil {
-		t.Error(err.Error())
+	userInfo := UserInfo{
+		Username: "dog",
+		Password: "007",
+	}
+
+	requestData, _ := json.Marshal(userInfo)
+
+	response, err1 := http.Post(
+		"http://167.71.166.120:10010/gf/api/user/login",
+		"application/json",
+		bytes.NewBuffer(requestData))
+	if err1 != nil {
+		t.Error("Failed to Request. " + err1.Error())
 		return
 	}
+	defer response.Body.Close()
+
+	content, err2 := ioutil.ReadAll(response.Body)
+	if err2 != nil {
+		t.Error("Failed to Request. " + err2.Error())
+		return
+	}
+
+	str := (*string)(unsafe.Pointer(&content))
+	if strings.Contains(*str, "400") {
+		t.Error("Failed to Request. " + *str)
+		return
+	}
+	fmt.Println(*str)
 }

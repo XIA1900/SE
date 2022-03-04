022ffa48b6f8af1854cb52cbf84b80d0d1a96382
BoweiWu <91042301+TOMBowei@users.noreply.github.com>
2022-03-04 16:28:38 -0500

    unit test for community manage

--- GFBackend/test/CommunityService_test.go
+++ GFBackend/test/CommunityService_test.go
@@ -40,3 +40,69 @@ func TestGetCommunityByName(t *testing.T) {
 	}
 	fmt.Println(*str)
 }
+
+func TestUpdateCommunity(t *testing.T) {
+	type CommunityInfo struct {
+		ID          int    `json:"ID"`
+		Name        string `json:"Name"`
+		Description string `json:"Description"`
+	}
+
+	communityInfo := CommunityInfo{
+		ID:          11,
+		Name:        "group11",
+		Description: "test11",
+	}
+
+	requestData, _ := json.Marshal(communityInfo)
+	response, err1 := http.NewRequest("POST", "http://localhost:10010/gf/api/community/updatecommunitybyid",
+		strings.NewReader(string(requestData)))
+	if err1 != nil {
+		t.Error("Failed to Request. " + err1.Error())
+	}
+	defer response.Body.Close()
+
+	content, err2 := ioutil.ReadAll(response.Body)
+	if err2 != nil {
+		t.Error("Failed to Read Response Body. " + err2.Error())
+		return
+	}
+
+	str := (*string)(unsafe.Pointer(&content))
+	if strings.Contains(*str, "400") {
+		t.Error("Failed to Update Community. " + *str)
+		return
+	}
+	fmt.Println(*str)
+}
+
+func TestDeleteCommunity(t *testing.T) {
+	type CommunityInfo struct {
+		ID int `json:"ID"`
+	}
+
+	communityInfo := CommunityInfo{
+		ID: 11,
+	}
+
+	requestData, _ := json.Marshal(communityInfo)
+	response, err1 := http.NewRequest("POST", "http://localhost:10010/gf/api/community/deletecommunitybyid",
+		strings.NewReader(string(requestData)))
+	if err1 != nil {
+		t.Error("Failed to Request. " + err1.Error())
+	}
+	defer response.Body.Close()
+
+	content, err2 := ioutil.ReadAll(response.Body)
+	if err2 != nil {
+		t.Error("Failed to Read Response Body. " + err2.Error())
+		return
+	}
+
+	str := (*string)(unsafe.Pointer(&content))
+	if strings.Contains(*str, "400") {
+		t.Error("Failed to Delete Community. " + *str)
+		return
+	}
+	fmt.Println(*str)
+}

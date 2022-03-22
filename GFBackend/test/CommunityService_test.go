5366929d720b70604fa9b86018eadd9dc7ee863f
Bowei_Wu <596746772@qq.com>
2022-03-21 20:45:44 -0400

    create community unit test pass

--- GFBackend/test/CommunityService_test.go
+++ GFBackend/test/CommunityService_test.go
@@ -106,3 +106,36 @@ func TestDeleteCommunity(t *testing.T) {
 	}
 	fmt.Println(*str)
 }
+
+func TestCreateCommunity(t *testing.T) {
+	type CommunityInfo struct {
+		Name        string `json:"Name"`
+		Description string `json:"Description"`
+	}
+
+	communityInfo := CommunityInfo{
+		Name:        "group11",
+		Description: "test11",
+	}
+
+	requestData, _ := json.Marshal(communityInfo)
+	response, err1 := http.NewRequest("POST", "http://localhost:10010/gf/api/community/create",
+		strings.NewReader(string(requestData)))
+	if err1 != nil {
+		t.Error("Failed to Request. " + err1.Error())
+	}
+	defer response.Body.Close()
+
+	content, err2 := ioutil.ReadAll(response.Body)
+	if err2 != nil {
+		t.Error("Failed to Read Response Body. " + err2.Error())
+		return
+	}
+
+	str := (*string)(unsafe.Pointer(&content))
+	if strings.Contains(*str, "400") {
+		t.Error("Failed to Create Community. " + *str)
+		return
+	}
+	fmt.Println(*str)
+}

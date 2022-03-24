59257a66d272f75bcc1c36f1bfd26f24211fdf0b
Bowei_Wu <596746772@qq.com>
2022-03-24 12:23:16 -0400

    get number of member in one community unit test pass

--- GFBackend/test/CommunityService_test.go
+++ GFBackend/test/CommunityService_test.go
@@ -203,3 +203,34 @@ func TestUpdateDescriptionByID(t *testing.T) {
 	}
 	fmt.Println(*str)
 }
+
+func TestGetNumberOfMemberByID(t *testing.T) {
+	type CommunityInfo struct {
+		ID int `json:"ID"`
+	}
+
+	communityInfo := CommunityInfo{
+		ID: 11,
+	}
+
+	requestData, _ := json.Marshal(communityInfo)
+	response, err1 := http.NewRequest("POST", "http://localhost:10010/gf/api/community/numberofmember/:id",
+		strings.NewReader(string(requestData)))
+	if err1 != nil {
+		t.Error("Failed to Request. " + err1.Error())
+	}
+	defer response.Body.Close()
+
+	content, err2 := ioutil.ReadAll(response.Body)
+	if err2 != nil {
+		t.Error("Failed to Read Response Body. " + err2.Error())
+		return
+	}
+
+	str := (*string)(unsafe.Pointer(&content))
+	if strings.Contains(*str, "400") {
+		t.Error("Failed to Get Number Of Member By ID. " + *str)
+		return
+	}
+	fmt.Println(*str)
+}

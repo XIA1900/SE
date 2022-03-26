f234fe932112f72c012d865ae1bac9538381d7f6
Bowei_Wu <596746772@qq.com>
2022-03-26 14:01:52 -0400

    leave community by id unit test pass

--- GFBackend/test/CommunityService_test.go
+++ GFBackend/test/CommunityService_test.go
@@ -327,3 +327,34 @@ func TestJoinCommunityByID(t *testing.T) {
 	}
 	fmt.Println(*str)
 }
+
+func TestLeaveCommunityByID(t *testing.T) {
+	type LeaveCommunity struct {
+		ID int `json:"ID"`
+	}
+
+	communityInfo := LeaveCommunity{
+		ID: 11,
+	}
+
+	requestData, _ := json.Marshal(communityInfo)
+	response, err1 := http.NewRequest("POST", "http://localhost:10010/gf/api/community/leave/:id",
+		strings.NewReader(string(requestData)))
+	if err1 != nil {
+		t.Error("Failed to Request. " + err1.Error())
+	}
+	defer response.Body.Close()
+
+	content, err2 := ioutil.ReadAll(response.Body)
+	if err2 != nil {
+		t.Error("Failed to Read Response Body. " + err2.Error())
+		return
+	}
+
+	str := (*string)(unsafe.Pointer(&content))
+	if strings.Contains(*str, "400") {
+		t.Error("Failed to Leave Community By ID. " + *str)
+		return
+	}
+	fmt.Println(*str)
+}

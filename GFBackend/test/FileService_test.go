2080b4415f1111abcd2fe860e90d86080fe6bf1e
fongziyjun16 <fongziyjun16@hotmail.com>
2022-03-29 16:38:15 -0400

    update user files test

--- GFBackend/test/FileService_test.go
+++ GFBackend/test/FileService_test.go
@@ -1,1 +1,21 @@
 package test
+
+import "testing"
+
+func TestGetSpaceInfo(t *testing.T) {
+}
+
+func TestExpandSpace(t *testing.T) {
+}
+
+func TestUploadFile(t *testing.T) {
+}
+
+func TestScanFiles(t *testing.T) {
+}
+
+func TestDownloadFile(t *testing.T) {
+}
+
+func TestDeleteFile(t *testing.T) {
+}

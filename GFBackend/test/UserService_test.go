66a7f037f413190b44000d0022ebac0073a692e2
fongziyjun16 <fongziyjun16@hotmail.com>
2022-03-29 16:29:10 -0400

    update user test

--- GFBackend/test/UserService_test.go
+++ GFBackend/test/UserService_test.go
@@ -64,6 +64,53 @@ func TestUserRegister(t *testing.T) {
 }
 
 func TestUserUpdatePassword(t *testing.T) {
+	loginInfo, err := userLogin("kirby", "008")
+	if err != nil || loginInfo.Message == "" {
+		t.Error("Fail to Login. Error Message: " + err.Error())
+		return
+	}
+	cookie := &http.Cookie{
+		Name:  "token",
+		Value: loginInfo.Message,
+	}
+	userInfo := entity.UserInfo{
+		Username:    "kirby",
+		Password:    "008",
+		NewPassword: "007",
+	}
+
+	requestData, _ := json.Marshal(userInfo)
+
+	request, err1 := http.NewRequest(
+		"POST",
+		"http://"+IP+":10010/gf/api/user/password",
+		bytes.NewBuffer(requestData))
+	if err1 != nil {
+		t.Error("Failed to Generate Request: " + err1.Error())
+		return
+	}
+	request.AddCookie(cookie)
+	jar, err2 := cookiejar.New(nil)
+	if err2 != nil {
+		t.Error("Failed to Set Cookie: " + err2.Error())
+		return
+	}
+	var client http.Client
+	client = http.Client{
+		Jar: jar,
+	}
+	response, err3 := client.Do(request)
+	if err3 != nil {
+		t.Error("Failed to Request: " + err3.Error())
+		return
+	}
+	defer response.Body.Close()
+
+	err4 := printResponseContent(response)
+	if err4 != nil {
+		t.Error("Failed to Interpret Response Message: " + err4.Error())
+		return
+	}
 
 }
 

dc9b2a4ebc59edbe600237353621761da75e7a11
fongziyjun16 <fongziyjun16@hotmail.com>
2022-03-29 17:14:17 -0400

    update

--- GFBackend/test/FileService_test.go
+++ GFBackend/test/FileService_test.go
@@ -69,14 +69,14 @@ func TestExpandSpace(t *testing.T) {
 
 	info := Info{
 		Username: "kirby",
-		Capacity: 36.6,
+		Capacity: 36.8,
 	}
 
 	requestData, _ := json.Marshal(info)
 
 	request, err1 := http.NewRequest(
 		"POST",
-		"http://"+"localhost"+":10010/gf/api/file/space/update",
+		"http://"+IP+":10010/gf/api/file/space/update",
 		bytes.NewBuffer(requestData))
 	if err1 != nil {
 		t.Error("Failed to Generate Request: " + err1.Error())
@@ -106,14 +106,49 @@ func TestExpandSpace(t *testing.T) {
 	}
 }
 
-func TestUploadFile(t *testing.T) {
-}
-
 func TestScanFiles(t *testing.T) {
-}
+	loginInfo, err := userLogin("kirby", "007")
+	if err != nil || loginInfo.Message == "" {
+		t.Error("Fail to Login. Error Message: " + err.Error())
+		return
+	}
+	cookie := &http.Cookie{
+		Name:  "token",
+		Value: loginInfo.Message,
+	}
 
-func TestDownloadFile(t *testing.T) {
+	request, err1 := http.NewRequest(
+		"POST",
+		"http://"+IP+":10010/gf/api/file/scan",
+		nil)
+	if err1 != nil {
+		t.Error("Failed to Generate Request: " + err1.Error())
+		return
+	}
+	request.AddCookie(cookie)
+	jar, err2 := cookiejar.New(nil)
+	if err2 != nil {
+		t.Error("Failed to Set Cookie: " + err2.Error())
+		return
+	}
+	var client http.Client
+	client = http.Client{
+		Jar: jar,
+	}
+	response, err3 := client.Do(request)
+	if err3 != nil {
+		t.Error("Failed to Request: " + err3.Error())
+		return
+	}
+	defer response.Body.Close()
+
+	err4 := printResponseContent(response)
+	if err4 != nil {
+		t.Error("Failed to Interpret Response Message: " + err4.Error())
+		return
+	}
 }
 
 func TestDeleteFile(t *testing.T) {
+
 }

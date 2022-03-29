c3e345704cc108e118774a59a035a82266413a8e
Bowei_Wu <596746772@qq.com>
2022-03-29 14:16:29 -0400

    article service test unit test finish pass

--- GFBackend/test/ArticleService_test.go
+++ GFBackend/test/ArticleService_test.go
@@ -50,3 +50,123 @@ func TestCreateArticle(t *testing.T) {
 	}
 	fmt.Println(*str)
 }
+
+func TestDeleteArticleByID(t *testing.T) {
+	type DeleteArticleByIDTest struct {
+		ID int
+	}
+	ArticleInfo := DeleteArticleByIDTest{
+		ID: 1,
+	}
+	requestData, _ := json.Marshal(ArticleInfo)
+	response, err1 := http.NewRequest("POST", "http://localhost:10010/gf/api/article/delete/:id",
+		strings.NewReader(string(requestData)))
+	if err1 != nil {
+		t.Error("Failed to Request. " + err1.Error())
+	}
+	defer response.Body.Close()
+
+	content, err2 := ioutil.ReadAll(response.Body)
+	if err2 != nil {
+		t.Error("Failed to Read Response Body. " + err2.Error())
+		return
+	}
+
+	str := (*string)(unsafe.Pointer(&content))
+	if strings.Contains(*str, "400") {
+		t.Error("Failed to Join Community By ID. " + *str)
+		return
+	}
+	fmt.Println(*str)
+}
+
+func TestUpdateArticleTitleOrContentByID(t *testing.T) {
+	type UpdateArticleTitleOrContentByIDTest struct {
+		ID      int
+		Title   string
+		Content string
+	}
+	ArticleInfo := UpdateArticleTitleOrContentByIDTest{
+		ID:      1,
+		Title:   "test",
+		Content: "test",
+	}
+	requestData, _ := json.Marshal(ArticleInfo)
+	response, err1 := http.NewRequest("POST", "http://localhost:10010/gf/api/article/update",
+		strings.NewReader(string(requestData)))
+	if err1 != nil {
+		t.Error("Failed to Request. " + err1.Error())
+	}
+	defer response.Body.Close()
+
+	content, err2 := ioutil.ReadAll(response.Body)
+	if err2 != nil {
+		t.Error("Failed to Read Response Body. " + err2.Error())
+		return
+	}
+
+	str := (*string)(unsafe.Pointer(&content))
+	if strings.Contains(*str, "400") {
+		t.Error("Failed to Join Community By ID. " + *str)
+		return
+	}
+	fmt.Println(*str)
+}
+
+func TestGetOneArticleByID(t *testing.T) {
+	type GetOneArticleByIDTest struct {
+		ID int
+	}
+	ArticleInfo := GetOneArticleByIDTest{
+		ID: 1,
+	}
+	requestData, _ := json.Marshal(ArticleInfo)
+	response, err1 := http.NewRequest("POST", "http://localhost:10010/gf/api/article/getone",
+		strings.NewReader(string(requestData)))
+	if err1 != nil {
+		t.Error("Failed to Request. " + err1.Error())
+	}
+	defer response.Body.Close()
+
+	content, err2 := ioutil.ReadAll(response.Body)
+	if err2 != nil {
+		t.Error("Failed to Read Response Body. " + err2.Error())
+		return
+	}
+
+	str := (*string)(unsafe.Pointer(&content))
+	if strings.Contains(*str, "400") {
+		t.Error("Failed to Join Community By ID. " + *str)
+		return
+	}
+	fmt.Println(*str)
+}
+
+func TestGetArticlesBySearchWords(t *testing.T) {
+	type GetArticlesBySearchWordsTest struct {
+		SearchWords string
+	}
+	ArticleInfo := GetArticlesBySearchWordsTest{
+		SearchWords: "test",
+	}
+	requestData, _ := json.Marshal(ArticleInfo)
+	response, err1 := http.NewRequest("POST", "http://localhost:10010/gf/api/article/search",
+		strings.NewReader(string(requestData)))
+	if err1 != nil {
+		t.Error("Failed to Request. " + err1.Error())
+	}
+	defer response.Body.Close()
+
+	content, err2 := ioutil.ReadAll(response.Body)
+	if err2 != nil {
+		t.Error("Failed to Read Response Body. " + err2.Error())
+		return
+	}
+
+	str := (*string)(unsafe.Pointer(&content))
+	if strings.Contains(*str, "400") {
+		t.Error("Failed to Join Community By ID. " + *str)
+		return
+	}
+	fmt.Println(*str)
+}

416c7d358601ec6b781965e55c53b74443550dd4
BoweiWu <91042301+TOMBowei@users.noreply.github.com>
2022-04-02 20:46:44 -0400

    get article list unit test pass

--- GFBackend/test/ArticleService_test.go
+++ GFBackend/test/ArticleService_test.go
@@ -170,3 +170,32 @@ func TestGetArticlesBySearchWords(t *testing.T) {
 	}
 	fmt.Println(*str)
 }
+
+func TestGetArticleList(t *testing.T) {
+	type GetArticleListTest struct {
+		Page int
+	}
+	ArticleInfo := GetArticleListTest{
+		Page: 1,
+	}
+	requestData, _ := json.Marshal(ArticleInfo)
+	response, err1 := http.NewRequest("POST", "http://localhost:10010/gf/api/article/getarticlelist",
+		strings.NewReader(string(requestData)))
+	if err1 != nil {
+		t.Error("Failed to Request. " + err1.Error())
+	}
+	defer response.Body.Close()
+
+	content, err2 := ioutil.ReadAll(response.Body)
+	if err2 != nil {
+		t.Error("Failed to Read Response Body. " + err2.Error())
+		return
+	}
+
+	str := (*string)(unsafe.Pointer(&content))
+	if strings.Contains(*str, "400") {
+		t.Error("Failed to Join Community By ID. " + *str)
+		return
+	}
+	fmt.Println(*str)
+}

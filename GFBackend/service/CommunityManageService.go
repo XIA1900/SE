5e029a176cc5892d290dba9fbe8987c359732425
BoweiWu <91042301+TOMBowei@users.noreply.github.com>
2022-04-13 13:04:46 -0400

    whether current user is in the group

--- GFBackend/service/CommunityManageService.go
+++ GFBackend/service/CommunityManageService.go
@@ -136,12 +136,12 @@ func (communityManageService *CommunityManageService) GetOneCommunityByID(id int
 	community, err1 := communityManageService.communityDAO.GetOneCommunityByID(id)
 	if err1 != nil {
 		logger.AppLogger.Error(err1.Error())
-		return entity.Community{}, 0, true, err1
+		return entity.Community{}, 0, false, err1
 	}
 	count, err2 := communityManageService.communityMemberDAO.CountMemberByCommunityID(id)
 	if err2 != nil {
 		logger.AppLogger.Error(err2.Error())
-		return entity.Community{}, 0, true, err2
+		return entity.Community{}, 0, false, err2
 	}
 	memberList, err3 := communityManageService.communityMemberDAO.GetMembersByCommunityIDs(id, (pageNO-1)*pageSize, pageSize)
 	if err3 != nil {
@@ -152,7 +152,7 @@ func (communityManageService *CommunityManageService) GetOneCommunityByID(id int
 		if memberList[i].Member == username {
 			return community, count, true, nil
 		} else {
-			return community, count, true, nil
+			return community, count, false, nil
 		}
 	}
 	return community, count, true, nil

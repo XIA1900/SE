5e504a2a309956f5f776108625e2cc553568755b
Kaiyue Wang <90939944+kywithsweet@users.noreply.github.com>
2022-04-20 22:29:29 -0400

    Update test.js

--- GFfrontend/cypress/integration/test.js
+++ GFfrontend/cypress/integration/test.js
@@ -4,27 +4,18 @@ Cypress.on('uncaught:exception', (err, runnable) => {
 describe('login', function(){
     this.beforeEach(()=>{
         cy.visit('http://localhost:8000/user/login')
-        cy.visit('http://localhost:8000/user/login')
         cy.get('.ant-pro-form-login-main').get('#username').type('cat')
         cy.get('.ant-pro-form-login-main').get('#password').type('007')
         cy.get('button:contains("Login")').click()
         cy.url().should('include', '/homepage')
-        cy.url().should('eq', 'http://localhost:8000/homepage')
         })
-
-    /*it('correctLogin', function(){
-        cy.get('.ant-pro-form-login-main').get('#username').type('kirby')
-        cy.get('.ant-pro-form-login-main').get('#password').type('007')
-        cy.get('button:contains("Login")').click()
-        cy.url().should('include', '/homepage')
-    })*/
     
     it('incorrectLogin', function(){
        cy.get('.ant-pro-form-login-main').get('#username').type('cat')
        cy.get('.ant-pro-form-login-main').get('#password').type('1234')
        cy.get('button:contains("Login")').click()
        cy.url().should('include', '/user/login') 
-    })
+     })
     
     it('forwardPersonalpage', function () {
         cy.contains('link').click()
@@ -37,7 +28,7 @@ describe('login', function(){
     it('forwardPostpage', function(){
         cy.contains('banan').click()
         cy.url().should('include', '/group/post?22')
-        })
+     })
     it('forwardGrouppage', function () {
         cy.contains('Apple').click()
         cy.url().should('include', '/group/content?22')
@@ -48,5 +39,35 @@ describe('login', function(){
             //submit
         cy.get('button:contains("Quit")').click()
         cy.url().should('eq', 'http://localhost:8000/group/content?22')
+     })
+    
+    it('forwardPersonalCenterpage', function () {
+        cy.contains('cat').click()
+        cy.contains('Personal Center').click()
+        cy.url().should('include', '/account/center?cat')
+     })
+   
+    it('forwardSettingspage', function () {
+        cy.contains('cat').click()
+        cy.contains('Settings').click()
+        cy.url().should('include', '/account/settings?cat')
+     })
+
+    it('forwardCreateGrouppage', function () {
+        cy.contains('cat').click()
+        cy.contains('Created Groups').click()
+        cy.url().should('include', '/account/selectGroups/created?cat')
+        //create
+     })
+    it('forwardJoinGrouppage', function () {
+        cy.contains('cat').click()
+        cy.contains('Created Groups').click()
+        cy.url().should('include', '/account/selectGroups/joined?cat')   
+     })
+
+    it('Logout', function () {
+        cy.contains('cat').click()
+        cy.contains('Log out').click()
+        cy.url().should('include', '/user/login')  
      })
 })
